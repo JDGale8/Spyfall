1f241e3d5abe268fa9e93354717135626e1a9387
Jake <Jake@JakeGale.com>
2016-11-14 23:03:14 +0000

    adds clear screen function and removes superfluous notes

--- Spyfall.py
+++ Spyfall.py
@@ -1,10 +1,10 @@
 import random
 import os
 
-places = ['Airplane', 'Bank', 'Beach', 'Cathedral', 'Circus tent', 'Corporate Party', 'Crusader Army', 'Casino',
-          'Day Spa', 'Embassy', 'Hospital', 'Hotel', 'Military Base', 'Movie Studio', 'Ocean Liner', 'Passenger Train',
-          'Space Station', 'Restaurant', 'School', 'Service Station', 'Space Station', 'Submarine', 'Supermarket',
-          'Theatre', 'University', 'World War II Squad']
+
+def clear_screen():
+    os.system('CLS')
+    os.system('clear')
 
 
 def new_place(number_of_players, places):
@@ -14,27 +14,32 @@ def new_place(number_of_players, places):
     return player_places
 
 
-def game(number_of_players, places):
-    places = new_place(number_of_players)
-    print("initial")
-    print("places:" + str(len(places)))
-    for i in range(len(places)):
+def print_result(place):
+    if place =='You are the Spy':
+        print("\n\n#################    " + place + "    #################\n\n -- You are the spy. Tell no one.")
 
-        print("enumerate: " + str(len(places)) + " people left to play")
+    else:
+        print("\n\n#################    " + place + "    #################\n\n -- This is your location. Read it and"
+                                                    " don't tell anyone.")
+    input("if you have read it. Hit enter.")
+    clear_screen()
+
+
+def game(number_of_players, place_list):
+
+    places = new_place(number_of_players, place_list)
+    for i in range(len(places)):
         input("please hit enter to play..")
         integer = random.randint(0, len(places) - 1)
         place = places.pop(integer)
-        print("\n\n#################    " + place + "    #################\n\n -- This is your place. read it. Dont tell anyone. If you are the spy. Tell no one.")
-        print("enter number of places left: " + str(len(places)) + "... These places are " + str(places))
-        input("if you have read it. Hit enter.")
-        print('\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n')
-        os.system('CLS')
+        print_result(place)
 
 
 number_of_players = None
 
 play = True
 while play:
+    clear_screen()
     number_of_players = input("Please enter the number of players: ")
     try:
         number_of_players = int(number_of_players)
@@ -43,5 +48,12 @@ while play:
     
     if isinstance(number_of_players, int):
             play = False
+
+print('Let the game begin.')
+
+places = ['Airplane', 'Bank', 'Beach', 'Cathedral', 'Circus tent', 'Corporate Party', 'Crusader Army', 'Casino',
+          'Day Spa', 'Embassy', 'Hospital', 'Hotel', 'Military Base', 'Movie Studio', 'Ocean Liner', 'Passenger Train',
+          'Space Station', 'Restaurant', 'School', 'Service Station', 'Space Station', 'Submarine', 'Supermarket',
+          'Theatre', 'University', 'World War II Squad']
 
 game(number_of_players, places)
